58879e2511041447bef6ddcc10f083f15445815b
gufranalam226 <63334077+gufranalam226@users.noreply.github.com>
2024-01-04 23:42:59 +0530

    Add files via upload

---
+++ Air india airlines/entertainment/quiz/js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When Air India Airlines had started first operation?",
+    answer: "1932",
+    options: [
+      "1921",
+      "1978",
+      "1932",
+      "1990"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What was the first jet did Air India Airlines had?",
+    answer: "Boeing 707",
+    options: [
+      "Boeing 737",
+      "Italian Cosmo jet",
+      "Boeing 707",
+      "McDonnell Douglas DC-9"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What was the longest flight Air India Airlines served?",
+    answer: "San Francisco-Bengaluru",
+    options: [
+      "Europe-Sydney",
+      "Istanbul-China",
+      "San Francisco-Bengaluru",
+      "Portugal-Singapore"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the India's first commercial pilot license-holder?",
+    answer: "JRD Tata",
+    options: [
+      "JRD Tata",
+      "Milkha",
+      "Ratan tata",
+      "Michael"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many countries Air India Airlines serve?",
+    answer: "84",
+    options: [
+      "92",
+      "84",
+      "98",
+      "122"
+    ]
+  },
+  
+];
